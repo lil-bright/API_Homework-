290f68eb01076074533935ab0ff70d412f27d3ff
lil-bright <mindy.k.bright@gmail.com>
2020-04-25 21:15:05 -0500

    round 1

---
+++ API_HW_keys.sh
@@ -0,0 +1,6 @@
+export PLAID_CLIENT_ID="5e9906f9631dc90012e6d5a2"
+export PLAID_SECRET="723fdf371f5f5be15939fa6d10e1d2"
+export PLAID_PUBLIC_KEY='c5e502ccf5ef201c3760affa0d8c1d'
+export PLAID_PRODUCTS='transactions'
+export PLAID_COUNTRY_CODES= 'US'
+export PLAID_ENV='sandbox'
